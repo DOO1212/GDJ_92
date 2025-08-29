0c5bbb87024fe8f73197932ee469d125ee3b47f7
DOOHYEON <ejejgus@naver.com>
2025-08-29 17:39:31 +0900

    Gradle

---
+++ GDJ_92_Gradle/src/main/java/com/winter/app/board/notice/NoticeController.java
@@ -0,0 +1,15 @@
+package com.winter.app.board.notice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notice/**")
+public class NoticeController {
+	
+
+}
